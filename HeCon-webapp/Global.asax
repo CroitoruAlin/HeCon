1cf0f42ed1e2d5d39906d720c7afc3487a17700f
Alin <alincroitoru97@gmail.com>
2019-03-14 22:40:28 +0200

    First commit

---
+++ HeCon-webapp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HeCon_webapp.MvcApplication" Language="C#" %>
---
+++ HeCon-webapp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HeCon_webapp.Startup))]
+namespace HeCon_webapp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
