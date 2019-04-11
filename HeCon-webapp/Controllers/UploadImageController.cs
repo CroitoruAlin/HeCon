776174c89331596ab6e8dc055a104a66f19ff4cc
Qastel <34581614+Qastel@users.noreply.github.com>
2019-04-11 21:39:05 +0300

    upload image By razvan2

---
+++ HeCon-webapp/Controllers/UploadImageController.cs
@@ -0,0 +1,43 @@
+﻿using HeCon_webapp.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HeCon_webapp.Controllers
+{
+    public class UploadImageController : Controller
+    {
+        private ApplicationDbContext db = ApplicationDbContext.Create();
+
+        // GET: Image
+        [HttpGet]
+        [AllowAnonymous]
+        public ActionResult New()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult New(UploadImage imageModel)
+        {
+
+            string fileName = Path.GetFileNameWithoutExtension(imageModel.ImageFile.FileName);
+            string extension = Path.GetExtension(imageModel.ImageFile.FileName);
+            fileName = fileName + DateTime.Now.ToString("yy_mm_ss_fff") + extension;
+            imageModel.ImagePath = "~/UploadedImages/" + fileName;
+            fileName = Path.Combine(Server.MapPath("~/UploadedImages/"), fileName);
+            imageModel.ImageFile.SaveAs(fileName);
+
+            db.UploadImages.Add(imageModel);
+            db.SaveChanges();
+
+            ModelState.Clear();
+
+            return View();
+        }
+    }
+}
