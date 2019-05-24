de202bc7f971e95a7dc6f4a2055e401912a8b740
Belci <belcineanu.alexandru@gmail.com>
2019-05-24 21:14:07 +0300

    FIX: re-commit unversioned files

---
+++ HeCon-webapp/Controllers/XRayController.cs
@@ -0,0 +1,57 @@
+﻿using HeCon_webapp.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HeCon_webapp.Controllers
+{
+    [Authorize(Roles = "Administrator")]
+    public class XRayController : Controller
+    {
+        private ApplicationDbContext db = ApplicationDbContext.Create();
+        private PythonCaller pythonCaller = PythonCaller.getPythonCaller();
+
+        // GET: Image
+        [HttpGet]
+        //[AllowAnonymous]
+        public ActionResult New()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        //[AllowAnonymous]
+        public ActionResult New(XRay imageModel)
+        {
+
+            string fileName = Path.GetFileNameWithoutExtension(imageModel.ImageFile.FileName);
+            string extension = Path.GetExtension(imageModel.ImageFile.FileName);
+            fileName = fileName + DateTime.Now.ToString("yy_mm_ss_fff") + extension;
+            imageModel.ImagePath = "~/UploadedImages/" + fileName;
+            fileName = Path.Combine(Server.MapPath("~/UploadedImages/"), fileName);
+            imageModel.UserId = User.Identity.GetUserId();
+            imageModel.ImageFile.SaveAs(fileName);      
+            Prediction result = pythonCaller.callPython(fileName);
+            ModelState.Clear();
+            imageModel.Prediction = result;
+            imageModel.PermissionToDoctor = 0;
+            ViewBag.result = result;
+            db.XRays.Add(imageModel);
+            db.SaveChanges();
+            return View();
+        }
+
+        public ActionResult Show()
+        {
+            var userId = User.Identity.GetUserId();
+            var xrays = db.XRays.Where(u => u.UserId == userId).Select(gr => gr);
+
+            ViewBag.XraysList = xrays;
+            return View();
+        }
+    }
+}
---
+++ HeCon-webapp/Models/XRay.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace HeCon_webapp.Models
+{
+    public class XRay
+    {
+        [Key]
+        public int ImageId { get; set; }
+
+        public string UserId { get; set; }
+
+        public string Title { get; set; }
+
+        [DisplayName("Upload File")]
+        public string ImagePath { get; set; }
+
+        [NotMapped]
+        public HttpPostedFileBase ImageFile { get; set; }
+
+        public Prediction Prediction { get; set; }
+
+        public int PermissionToDoctor {get;set;}
+    }
+}
